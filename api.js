fe3655386d2056e469b5f6abb8896fc5e743d786
Alex <alexrumer@gmail.com>
2024-01-13 12:15:16 -0800

    Added title case function
    -Added title case function and updated type and priority fields to use it.
    -Added comments

--- api.js
+++ api.js
@@ -1,140 +1,173 @@
+/**
+ * Defines all of the fields and creates payload to be sent to the API to create the issue
+ *
+ * @param {string} url URL of the API.
+ * @param {string} credentials Username + token encoded in base64.
+ * @param {array} dataArray Array which contains all the issue data.
+ * @param {int} issueIndex Index of the issue to be created in the dataArray.
+ * @return the status of the issue from sendAPIData.
+ */
 function createIssue (url, credentials, dataArray, issueIndex){
-    const sheet = SpreadsheetApp.getActiveSheet();
-    proj = dataArray["project"][issueIndex];
-    description = dataArray["description"][issueIndex];
-    summary = dataArray["summary"][issueIndex];
-    issueType = dataArray["type"][issueIndex];
-    if (issueType ==""){
-      issueType = sheet.getRange("defaultType").getValue();
-      sheet.getRange("hType").offset(issueIndex + 1,0).setValue(issueType);
-    }
-    priority = dataArray["priority"][issueIndex];
-    if (priority ==""){
-      priority = sheet.getRange("defaultPriority").getValue();
-      sheet.getRange("hPriority").offset(issueIndex + 1,0).setValue(priority);
-    }
-    var data = {}
+  const sheet = SpreadsheetApp.getActiveSheet();
+  proj = dataArray["project"][issueIndex];
+  var data = {}
+
+  //initalize fields
+  description = dataArray["description"][issueIndex];
+  summary = dataArray["summary"][issueIndex];
   
-    data = {
-      "fields": {
-        "project":{ 
-            "key": proj
-        },
-        "priority": {
-            "name": priority
-        },
-        "summary": summary,
-        "description": description,
-        "issuetype":{
-            "name": issueType
-        }
+  issueType = dataArray["type"][issueIndex];
+  if (issueType ==""){
+    issueType = sheet.getRange("defaultType").getValue();
+    sheet.getRange("hType").offset(issueIndex + 1,0).setValue(issueType);
+  }
+  issueType = toTitle(issueType); //convert to Proper case due to API case sensitivty
+ 
+  priority = dataArray["priority"][issueIndex];
+  if (priority ==""){
+    priority = sheet.getRange("defaultPriority").getValue();
+    sheet.getRange("hPriority").offset(issueIndex + 1,0).setValue(priority);
+  }
+  priority = toTitle(priority);
+
+  //build data array
+  data = {
+    "fields": {
+      "project":{ 
+          "key": proj
+      },
+      "priority": {
+          "name": priority
+      },
+      "summary": summary,
+      "description": description,
+      "issuetype":{
+          "name": issueType
       }
-    };
-  
-    var payload = JSON.stringify(data);
-  
-    return sendAPIData(url, credentials, "POST", payload);
-  }
-  
-  function updateIssue (url, credentials, dataArray, issueIndex){
-    const sheet = SpreadsheetApp.getActiveSheet();
-    url = url + dataArray["key"][issueIndex] + "?returnIssue=True";
-    description = dataArray["description"][issueIndex];
-    summary = dataArray["summary"][issueIndex];
-    priority = dataArray["priority"][issueIndex];
-    if (priority ==""){
-      priority = sheet.getRange("defaultPriority").getValue();
-      sheet.getRange("hPriority").offset(issueIndex + 1,0).setValue(priority);
     }
-    var data = {}
-  
-    data = {
-      "fields": {
-        "priority": {
-            "name": priority
-        },
-        "summary": summary,
-        "description": description
-      }
-    };
-  
-    var payload = JSON.stringify(data);
-  
-    return sendAPIData(url, credentials, "PUT", payload);
+  };
+
+  var payload = JSON.stringify(data);
+
+  return sendAPIData(url, credentials, "POST", payload);
+}
+/**
+ * Defines all of the fields and creates payload to be sent to the API to update an issue.
+ *
+ * @param {string} url URL of the API.
+ * @param {string} credentials Username + token encoded in base64.
+ * @param {array} dataArray Array which contains all the issue data.
+ * @param {int} issueIndex Index of the issue to be created in the dataArray.
+ * @return the status of the issue from sendAPIData.
+ */
+function updateIssue (url, credentials, dataArray, issueIndex){
+  const sheet = SpreadsheetApp.getActiveSheet();
+  url = url + dataArray["key"][issueIndex] + "?returnIssue=True";
+  description = dataArray["description"][issueIndex];
+  summary = dataArray["summary"][issueIndex];
+  priority = dataArray["priority"][issueIndex];
+  if (priority ==""){
+    priority = sheet.getRange("defaultPriority").getValue();
+    sheet.getRange("hPriority").offset(issueIndex + 1,0).setValue(priority);
   }
-  
-  function sendAPIData(url, credentials, method="GET", payload = ""){
-    try {
-      var responseMap = {};
-      var headers = 
-        { 
-          "content-type": "application/json",
-          "Accept": "application/json",
-          "authorization": "Basic " + credentials
-        }; 
-      
-      var options = 
-        { 
-          "content-type": "application/json",
-          "method": method,
-          "headers": headers,
-          "payload": payload
-        };  
-  
-      //
-      // Make the HTTP call to the JIRA API
-      //
-      var response = UrlFetchApp.fetch(url, options);
-      Logger.log(response.getContentText());
-  
-      var dataAll = JSON.parse(response.getContentText());
-      
-      Logger.log(dataAll);
-  
-      var responseMap = {};
-      responseMap["key"] = dataAll.key;
-      responseMap["code"] = response.getResponseCode();
-      return responseMap
-    }catch(e){
-      Logger.log("issue failed");
-      responseMap["e"] = e;
-      responseMap["code"] = 400;
-      return responseMap
+  var data = {}
+
+  data = {
+    "fields": {
+      "priority": {
+          "name": priority
+      },
+      "summary": summary,
+      "description": description
     }
-  }
-  
-  
-  //gets the user's Jira username and API key information
-  function getLogin() {
-    var ui = SpreadsheetApp.getUi();
-  
-    //get username
-    var username = ui.prompt("Please enter Jira Username",ui.ButtonSet.OK_CANCEL);
+  };
+
+  var payload = JSON.stringify(data);
+
+  return sendAPIData(url, credentials, "PUT", payload);
+}
+
+/**
+ * Sends payload data to the Jira API.
+ *
+ * @param {string} url URL of the API.
+ * @param {string} credentials Username + token encoded in base64.
+ * @param {string} method The HTTP method to be used (PUT, GET, POST, etc).
+ * @param {array} payload Payload to be sent to the API.
+ * @return The status response in the form of a map [key, code, e]. Key=Jira Key, code=HTTP respose, e=error if any.
+ */
+function sendAPIData(url, credentials, method="GET", payload = ""){
+  try {
+    var responseMap = {};
+    var headers = 
+      { 
+        "content-type": "application/json",
+        "Accept": "application/json",
+        "authorization": "Basic " + credentials
+      }; 
     
-    if (username.getResponseText().length > 0) {
-      Logger.log("User provided username");
-      Logger.log(username.getResponseText());
-    } else {
-      Logger.log("ERROR: User aborted username"); 
-      return false;
-    }
-  
-    //get API key
-    var apitoken= ui.prompt("Please enter Jira API key",ui.ButtonSet.OK_CANCEL);
+    var options = 
+      { 
+        "content-type": "application/json",
+        "method": method,
+        "headers": headers,
+        "payload": payload
+      };  
+
+    //
+    // Make the HTTP call to the JIRA API
+    //
+    var response = UrlFetchApp.fetch(url, options);
+    Logger.log(response.getContentText());
+
+    var dataAll = JSON.parse(response.getContentText());
     
-    if (apitoken.getResponseText().length > 0) {
-      Logger.log("User provided API Token");
-    } else {
-      Logger.log("ERROR: User didn't provide key"); 
-      return false;
-    }
-    var test =  username.getResponseText() + ':' + apitoken.getResponseText();
-    return Utilities.base64Encode(test) 
+    Logger.log(dataAll);
+
+    var responseMap = {};
+    responseMap["key"] = dataAll.key;
+    responseMap["code"] = response.getResponseCode();
+    return responseMap
+  }catch(e){
+    Logger.log("issue failed");
+    responseMap["e"] = e;
+    responseMap["code"] = 400;
+    return responseMap
   }
+}
+
+
+/**
+ * gets user Login info using two prompts
+ *
+ * @customfunction
+ * @return Returns the encoded base64 username + API token key. If error, this returns false. 
+ */
+function getLogin() {
+  var ui = SpreadsheetApp.getUi();
+
+  //get username
+  var username = ui.prompt("Please enter Jira Username",ui.ButtonSet.OK_CANCEL);
   
+  if (username.getResponseText().length > 0) {
+    Logger.log("User provided username");
+    Logger.log(username.getResponseText());
+  } else {
+    Logger.log("ERROR: User aborted username"); 
+    return false;
+  }
+
+  //get API key
+  var apitoken= ui.prompt("Please enter Jira API key",ui.ButtonSet.OK_CANCEL);
   
-  
-  
-  //
-  
-   
+  if (apitoken.getResponseText().length > 0) {
+    Logger.log("User provided API Token");
+  } else {
+    Logger.log("ERROR: User didn't provide key"); 
+    return false;
+  }
+  var test =  username.getResponseText() + ':' + apitoken.getResponseText();
+  return Utilities.base64Encode(test) 
+}
+
+ 
--- main.js
+++ main.js
@@ -1,109 +1,129 @@
 function main() {
-    const sheet = SpreadsheetApp.getActiveSheet();
+  const sheet = SpreadsheetApp.getActiveSheet();
+  
+  Logger.log("Version: " + sheet.getRange("version").getValue());
+  //read sheet data
+  setStatus('Reading data...');
+  var dataArray = readSheetData("rSheetData");
+
+  var urlSubDomain = sheet.getRange("jirasubdomain").getValue();
+  var urlHTTPIssue = "https://" + urlSubDomain + ".atlassian.net/browse/"
+  var urlIssue = "https://" + urlSubDomain + ".atlassian.net/rest/api/2/issue/";
+  var numDataRows = sheet.getRange("numRows").getValue();
+
+  setStatus('Data read');
+  creds = getLogin();
+  if (creds == false){
+    setStatus('Login cancelled',true);
+    return false
+  }
+
+  clearData(false);
+
+  var response = {};
+  for (var i = 0; i <= numDataRows; i++)
+
     
-    Logger.log("Version: " + sheet.getRange("version").getValue());
-    //read sheet data
-    setStatus('Reading data...');
-    var dataArray = readSheetData("rSheetData");
-  
-    var urlSubDomain = sheet.getRange("jirasubdomain").getValue();
-    var urlHTTPIssue = "https://" + urlSubDomain + ".atlassian.net/browse/"
-    var urlIssue = "https://" + urlSubDomain + ".atlassian.net/rest/api/2/issue/";
-    var numDataRows = sheet.getRange("numRows").getValue();
-  
-    setStatus('Data read');
-    creds = getLogin();
-    if (creds == false){
-      setStatus('Login cancelled',true);
-      return false
-    }
-  
-    clearData(false);
-  
-    var response = {};
-    for (var i = 0; i <= numDataRows; i++)
-  
-      
-      if (dataArray["summary"][i] != "" && dataArray["skip"][i] == false ){
-        response = {}
-        response = createIssue(urlIssue, creds, dataArray, i)
-        if (response.code == 201){
-           setStatus("Created issue " + (i + 1) + " of " + numDataRows)
-          Logger.log(response.key);
-          dataArray["key"][i] = response.key;
-          sheet.getRange("hMessage").offset(i + 1, 0).setValue("Created!");
-          sheet.getRange("hSkip").offset(i + 1, 0).setValue("TRUE");
-          setCellURL(sheet.getRange("hKey").offset(i +1, 0), response.key, urlHTTPIssue)
-        }else{
-          Logger.log(response["e"]);
-          dataArray["message"][i] = response.e;
-          sheet.getRange("hMessage").offset(i + 1, 0).setValue(response.e)
-        }
-       
-      }else if (dataArray["summary"][i] != "" && dataArray["skip"][i] == true && dataArray["key"][i] != "" ){
-        response = {}
-        response = updateIssue(urlIssue, creds, dataArray, i)
-        if (response.code == 200){
-           setStatus("Updated issue " + (i + 1) + " of " + numDataRows)
-           sheet.getRange("hMessage").offset(i +1, 0).setValue("Updated!");
-        }else{
-          Logger.log(response["e"]);
-          dataArray["message"][i] = response["e"];
-          sheet.getRange("hMessage").offset(i + 1, 0).setValue(response.e)
-        }
+    if (dataArray["summary"][i] != "" && dataArray["skip"][i] == false ){
+      response = {}
+      response = createIssue(urlIssue, creds, dataArray, i)
+      if (response.code == 201){
+         setStatus("Created issue " + (i + 1) + " of " + numDataRows)
+        Logger.log(response.key);
+        dataArray["key"][i] = response.key;
+        sheet.getRange("hMessage").offset(i + 1, 0).setValue("Created!");
+        sheet.getRange("hSkip").offset(i + 1, 0).setValue("TRUE");
+        setCellURLKey(sheet.getRange("hKey").offset(i +1, 0), response.key, urlHTTPIssue)
+      }else{
+        Logger.log(response["e"]);
+        dataArray["message"][i] = response.e;
+        sheet.getRange("hMessage").offset(i + 1, 0).setValue(response.e)
       }
-  
-      setStatus("Done creating issues");
-  
-  }
-  
-  //reads the table data from the sheet and returns map of arrays with headers
-  function readSheetData (NamedDataRange = "rSheetData"){
-    
-    const sheet = SpreadsheetApp.getActiveSheet();
-    var sData = sheet.getRange(NamedDataRange).getValues();
-  
-    // Extract headers from https://stackoverflow.com/questions/62186607/how-to-convert-table-data-to-an-object-and-have-headers-as-keys
-    var headers = sData[0]; //header row
-    headers = headers.map(v => v.toString().toLowerCase());
-    var body = sData.slice(1); //rest of the data
-    var dataMap = {};
-    var i = 0;
-    // For each header
-    headers.forEach(function(col){''
-      // Create an empty list for the values
-      dataMap[col] = [];
-      // For every row in the body assign the values to their keys 
-      body.map(row => dataMap[col].push(row[i]));
-      i++;
-    })
-    return dataMap;
-  }
-  
-  function setStatus(message="", isError=false){
-    const sheet = SpreadsheetApp.getActiveSheet();
-    var status = sheet.getRange("status")
-    if (isError == true){
-      status.setValue(message)
-      .setBackground('#f4cccc');
-    }else if(message ==""){
-      status.clearFormat()
-      .clearContent();
-    }else{
-      status.setValue(message)
-      .setBackground('#fff2cc');
+     
+    }else if (dataArray["summary"][i] != "" && dataArray["skip"][i] == true && dataArray["key"][i] != "" ){
+      response = {}
+      response = updateIssue(urlIssue, creds, dataArray, i)
+      if (response.code == 200){
+         setStatus("Updated issue " + (i + 1) + " of " + numDataRows)
+         sheet.getRange("hMessage").offset(i +1, 0).setValue("Updated!");
+      }else{
+        Logger.log(response["e"]);
+        dataArray["message"][i] = response["e"];
+        sheet.getRange("hMessage").offset(i + 1, 0).setValue(response.e)
+      }
     }
-  }
+
+    setStatus("Done creating issues");
+
+}
+
+/**
+ * Reads the table data from the sheet and returns map of arrays with headers
+ *
+ * @param {range} NamedDataRange Range where the data to be read is stored, including headers
+ * @@return a dataMap with headers as keys and arrays as values. 
+ */
+function readSheetData (NamedDataRange = "rSheetData"){
   
-  function clearData(userClear=false){
-    const sheet = SpreadsheetApp.getActiveSheet();
-    if (userClear){
-      sheet.getRange("UserData").clearContent();
-    }
-    sheet.getRange("colMessage").clearContent();
-    setStatus();
+  const sheet = SpreadsheetApp.getActiveSheet();
+  var sData = sheet.getRange(NamedDataRange).getValues();
+
+  // Extract headers from https://stackoverflow.com/questions/62186607/how-to-convert-table-data-to-an-object-and-have-headers-as-keys
+  var headers = sData[0]; //header row
+  headers = headers.map(v => v.toString().toLowerCase());
+  var body = sData.slice(1); //rest of the data
+  var dataMap = {};
+  var i = 0;
+  // For each header
+  headers.forEach(function(col){''
+    // Create an empty list for the values
+    dataMap[col] = [];
+    // For every row in the body assign the values to their keys 
+    body.map(row => dataMap[col].push(row[i]));
+    i++;
+  })
+  return dataMap;
+}
+
+
+/**
+ * Display the status message in the status cell, and sets it to the appropriate color. 
+ *
+ * @param {string} message Message to be shown. If message is "". then cell color will be cleared.
+ * @param {bool} isError If true red color will be used, otherwise green color will be used.
+ * @customfunction
+ */
+function setStatus(message="", isError=false){
+  const sheet = SpreadsheetApp.getActiveSheet();
+  var status = sheet.getRange("status")
+  if (isError == true){
+    status.setValue(message)
+    .setBackground('#f4cccc'); //red
+  }else if(message ==""){
+    status.clearFormat() //clear cell color as there is no message
+    .clearContent(); 
+  }else{ 
+    status.setValue(message)
+    .setBackground('#fff2cc');
   }
-  function clearSheetButton(){
-    clearData(true);
+}
+
+/**
+ * Sets the sheet to the default state or just clears state data.
+ *
+ * @param {bool} userClear default false. true=clear all user data. false=clear only state data.
+ * @customfunction
+ */
+function clearData(userClear=false){
+  const sheet = SpreadsheetApp.getActiveSheet();
+  if (userClear){
+    sheet.getRange("UserData").clearContent();
   }
-  
+  sheet.getRange("colMessage").clearContent();
+  setStatus();
+}
+
+//function called by the user on the sheet via button to clear sheet
+function clearSheetButton(){
+  clearData(true);
+}
